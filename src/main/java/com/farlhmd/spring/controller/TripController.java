d8ce1479c6a05d7f084117b5767a07d4a83c3123
farlhmd <57334075+farlhmd@users.noreply.github.com>
2022-07-03 10:29:57 +0700

    Initial commit

---
+++ src/main/java/com/farlhmd/spring/controller/TripController.java
@@ -0,0 +1,65 @@
+package com.farlhmd.spring.controller;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.farlhmd.spring.model.Agency;
+import com.farlhmd.spring.model.Bus;
+import com.farlhmd.spring.model.Stop;
+import com.farlhmd.spring.model.Trip;
+import com.farlhmd.spring.payload.request.TripRequest;
+import com.farlhmd.spring.repository.AgencyRepository;
+import com.farlhmd.spring.repository.BusRepository;
+import com.farlhmd.spring.repository.StopRepository;
+import com.farlhmd.spring.repository.TripRepository;
+
+import io.swagger.annotations.*;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/v1/trip")
+public class TripController {
+	@Autowired
+	TripRepository tripRepository;
+
+	@Autowired
+	AgencyRepository agencyRepository;
+
+	@Autowired
+	BusRepository busRepository;
+
+	@Autowired
+	StopRepository stopRepository;
+
+	@PostMapping("/")
+	@ApiOperation(value = "", authorizations = { @Authorization(value = "apiKey") })
+	@PreAuthorize("hasRole('ADMIN')")
+	public ResponseEntity<?> addTrip(@Valid @RequestBody TripRequest tripRequest) {
+		Agency agency = agencyRepository.findById(tripRequest.getAgencyId()).get();
+		Bus bus = busRepository.findById(tripRequest.getBusId()).get();
+		Stop sourceStop = stopRepository.findById(tripRequest.getSourceStopId()).get();
+		Stop destStop = stopRepository.findById(tripRequest.getDestStopId()).get();
+		Trip trip = new Trip(tripRequest.getFare(), tripRequest.getJourneyTime(), sourceStop, destStop, bus, agency);
+		return ResponseEntity.ok(tripRepository.save(trip));
+	}
+
+	@GetMapping("/{id}")
+	@ApiOperation(value = "", authorizations = { @Authorization(value = "apiKey") })
+	@PreAuthorize("hasRole('ADMIN')")
+	public ResponseEntity<?> getTripByAgencyId(@PathVariable(value = "id") Long id) {
+		List<Trip> trip = tripRepository.findByAgencyId(id);
+		return ResponseEntity.ok(trip);
+	}
+}
